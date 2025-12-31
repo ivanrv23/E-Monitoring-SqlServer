9ea0c357f3d0fad1f59200f7309bcb7c181a3921
Iván Ramos <ronalirv23@gmail.com>
2025-12-31 12:56:19 -0500

    ajustando migraciones datetime

--- models/AcelerografoModel.py
+++ models/AcelerografoModel.py
@@ -312,7 +312,7 @@ class AcelerografoModel:
             CREATE TABLE {tabla} (
                 id_detalle INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
                 id_acelerografo INT NOT NULL,
-                fecha_detalle VARCHAR(50) NOT NULL,
+                fecha_detalle DATETIME NOT NULL,
                 magnitud_detalle FLOAT NOT NULL,
                 distancia_detalle FLOAT NOT NULL,
                 observacion_detalle VARCHAR(MAX)
